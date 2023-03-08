3277292f834a854e209c846a2e435b159c9ec8f7
keitA <106457391+jtatet@users.noreply.github.com>
2023-03-08 14:57:25 +0900

    Update index.js

--- Kcode!bate/Kcode_Debug_login/index.js
+++ Kcode!bate/Kcode_Debug_login/index.js
@@ -1,4 +1,8 @@
-function clickBtn1() {
-    const pass1 = document.getElementById("pass1");
-    document.getElementById("span1").textContent = pass1.value;
-  }
+function goLoginPage(){
+    passwd=document.formno1.passwd.value;
+    if(passwd=="tarako"){
+     location.href=".KAPI.html";
+    }else{
+     alert('パスワードが違います');
+    }
+   }
