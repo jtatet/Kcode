b83c35bf676a1c87a56026232b8e693b1294e7ff
keitA <106457391+jtatet@users.noreply.github.com>
2023-03-08 15:08:56 +0900

    Update index.js

--- Kcode!bate/Kcode_Debug_login/index.js
+++ Kcode!bate/Kcode_Debug_login/index.js
@@ -1,6 +1,6 @@
 function goLoginPage(){
     passwd=document.formno1.passwd.value;
-    if(passwd=="tarako"){
+    if(passwd=="K526078"){
      location.href=".KAPI.html";
     }else{
      alert('パスワードが違います');
