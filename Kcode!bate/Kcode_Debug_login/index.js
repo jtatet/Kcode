7e4a86f3e706eb78cb50403786e80ae4671e20c7
keitA <106457391+jtatet@users.noreply.github.com>
2023-03-09 22:26:52 +0900

    Add files via upload

---
+++ Kcode!bate/Kcode_Debug_login/index.js
@@ -0,0 +1,8 @@
+function goLoginPage(){
+    passwd=document.formno1.passwd.value;
+    if(passwd=="K526078"){
+     location.href="/Kcode/Kcode!bate/Kcode_Debug_login/KAPI.html";
+    }else{
+     alert('パスワードが違います');
+    }
+   }
