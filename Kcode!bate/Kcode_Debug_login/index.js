f96074ac4dcfb7ebc5089698a55bdf7886df9c57
keitA <106457391+jtatet@users.noreply.github.com>
2023-03-09 22:25:38 +0900

    Delete Kcode!bate/Kcode_Debug_login directory

--- Kcode!bate/Kcode_Debug_login/index.js
+++
@@ -1,8 +0,0 @@
-function goLoginPage(){
-    passwd=document.formno1.passwd.value;
-    if(passwd=="K526078"){
-     location.href="/Kcode/Kcode!bate/Kcode_Debug_login/KAPI.html";
-    }else{
-     alert('パスワードが違います');
-    }
-   }
